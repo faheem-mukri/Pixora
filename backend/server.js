942e16a94273cf4bb27062ade9d85ae7c13c44a9
faheem-mukri <faheem.work.dev@gmail.com>
2026-06-01 19:12:24 +0530

    Fix cookie parser import

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const helmet = require('helmet');
-const cookies = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
